4920b33993124e8d55abefbe4e42d6c573119b07
Takurreddy <takurmukku158@gmail.com>
2026-09-02 12:11:44 +0530

    fix: Hardcode Firebase config to resolve Vercel black screen

--- AQI-Project/frontend/src/firebase.js
+++ AQI-Project/frontend/src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyBk0b-frtBPWsZa2nlf7zuduXSlG2QkM2k",
+  authDomain: "air-aware-1f614.firebaseapp.com",
+  projectId: "air-aware-1f614",
+  storageBucket: "air-aware-1f614.firebasestorage.app",
+  messagingSenderId: "148135147817",
+  appId: "1:148135147817:web:b4f965a0caeee7ba115f38",
+  measurementId: "G-NRS3S2F9T1"
 };
 
 const app = initializeApp(firebaseConfig);
